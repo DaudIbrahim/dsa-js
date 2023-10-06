1e362e2774e4ae8bc89c62289e38841b81bf25ae
Mohammad Daud Ibrahim <daud.ibrahim@mds.com.bd>
2023-10-07 04:01:34 +0600

    Breadth-First-Search-Graph-Adjacency-Matrix-2D-Array

--- kata-machine/src/day2/BFSGraphMatrix.ts
+++ kata-machine/src/day2/BFSGraphMatrix.ts
@@ -1,11 +1,65 @@
 // WeightedAdjacencyMatrix are 2D Arrays
 
-export default function bfs(
-    graph: WeightedAdjacencyMatrix,
-    source: number,
-    needle: number)
-    : number[] | null {
+export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+    // [] -> enqueue, dequeue
+    const queue: number[] = [source]
 
+    // [idx0, idx1, idx2]
+    // [val2, val0, val1]
+    const previous = new Array(graph.length).fill(false);
 
-    
+    // [idx0, idx1, idx2]
+    // [true, false, true]
+    const seen = new Array(graph.length).fill(false);
+
+    // use seen, push value & revers in the end for correct order
+    const result: number[] = []
+
+    do {
+        const val = queue.shift()
+
+        if (val !== undefined && seen[val] === false) {
+
+            if (val === needle) {
+                result.push(val)
+                break
+            }
+
+            for (let idx = 0; idx < graph.length; idx++) {
+
+                // No need to compare the node with itself
+                if (val === idx) continue
+
+                if (graph[val][idx] >= 1 && seen[idx] === false) {
+                    queue.unshift(idx)
+                    previous[idx] = val
+                }
+            }
+
+            seen[val] = true
+        }
+
+    } while (queue.length);
+
+    if (result.length === 0) {
+        return null
+    }
+
+    // Generate previous, build it backwards
+    let num = result[0]
+
+    // While it has a previous node
+    while (previous[num]) {
+        num = previous[num]
+        result.push(num)
+    }
+
+    // Add the source.
+    // Source does not have a previous val thus it will neither the enter the while loop above nor push to the result
+    result.push(source)
+
+    // Reverse the result is in correct order
+    // With out reverse, result [xyz, source]
+    // With reverse, result [source, xyz]
+    return result.reverse()
 }
--- kata-machine/src/day2/BTBFS.ts
+++ kata-machine/src/day2/BTBFS.ts
@@ -25,3 +25,57 @@ export default function bfs(head: BinaryNode<number>, needle: number): boolean {
 
     return isFound
 }
+
+/**
+ * Memorize: Breadth First Search
+ * - Queue
+ * - FIFO
+ * - Vertically traversing nodes at each level so to speak
+ * 
+ * Memorize: Depth First Search
+ * - Stack
+ * - LIFO
+ * - Horizontally traversing nodes from level start to level end
+ */
+export function my_implementation_bfs_1(head: BinaryNode<number>, needle: number): boolean {
+    const q: [BinaryNode<number> | null] = [head]
+
+    while (q.length) {
+        const curr = q.shift()
+        if (!curr) continue
+        if (curr.value === needle) return true
+        q.push(curr.left)
+        q.push(curr.right)
+    }
+
+    return false
+}
+
+/**
+ * my_implementation
+ * I might have written code for breadth first traversal as seen in Reducible's YT Video
+ */
+export function my_implementation_bfs_0(head: BinaryNode<number>, needle: number): boolean {
+    const queue: [BinaryNode<number> | null] = [head]
+
+    while (queue.length) {
+        let tmpLength = queue.length
+
+        for (let i = 0; i < tmpLength; tmpLength--) {
+            const curr = queue[i];
+
+            if (curr === null) {
+                queue.shift()
+                continue
+            }
+
+            if (curr.value === needle) return true
+
+            queue.shift()
+            queue.push(curr.left)
+            queue.push(curr.right)
+        }
+    }
+
+    return false
+}

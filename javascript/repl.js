102f6703d4080ba387353c5efc4d6314441c0f91
Mohammad Daud Ibrahim <daud.ibrahim@mds.com.bd>
2023-12-25 08:37:25 +0600

    JS only

--- javascript/repl.js
+++ javascript/repl.js
@@ -1,17 +1,1 @@
-const bst = new BinarySearchTree()
-
-// insert
-bst.insert(10, 'A');
-bst.insert(5, 'B');
-bst.insert(15, 'C');
-bst.insert(3, 'D');
-bst.insert(8, 'E');
-bst.insert(12, 'F');
-bst.insert(18, 'G');
-
-// delete
-bst.delete(5);
-
-// walk
-console.log(bst.walk());
-
+// Repl JS
--- patterns/src/01 - Two Pointers/README.md
+++ patterns/src/01 - Two Pointers/README.md
@@ -3,5 +3,4 @@
 1. Running from both ends of an array
 2. Fast and Slow Pointers
 3. Running from beginning of 2 arrays / Merging 2 arrays
-
 4. Split & Merge of an array / Divide and Conquer
